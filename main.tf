f510c712c0d6278cf8d98d534c2710791b8c033d
Gabe Susman <gabe.susman@gmail.com>
2022-03-29 11:20:12 -0600

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform { 
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+}
+
+provider "aws" {
+  
+}
+
+// ecr - aws container repository
+resource "" "name" {
+  
+}
+
+// ec2 - this will be the dev server, it will host whats actually being served and the jenkins agent
+resource "" "name" {
+  
+}
+
+
+resource "" "name" {
+  
+}
